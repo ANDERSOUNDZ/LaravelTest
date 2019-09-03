7b7625243b994798b8d0d69e0e246368336cc2b0
nankingscolors <andersonmiko@live.com>
2019-09-02 23:56:39 -0500

    first Test

--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,19 @@
 
 Route::get('/', function () {
     
+    //--------------------------------------INICIO PRIMERA CLASE-------------------------------------
     //return "The new project Test";
     //return view('welcome');
 
+    /*
+    //-----INICIO INSERTAR REGRISTROS EN UNA BASE DE DATOS
     //Variables
     //Funcion bcrypt() - cifra las contraseñas, de manera obligatoria
+
     $password = bcrypt('1234');
     
     //Facade
     DB::table('users')->insert([
-
         
         ['name'=>'Anderson', 'email' =>'andersonmikol@live.com', 'password'=> $password],
         ['name'=>'Miguel', 'email' =>'miguelushina@live.com', 'password'=> $password]
@@ -32,5 +35,22 @@ Route::get('/', function () {
     ]);
 
     return 'Registro Creado';
+        
+    //-------FIN INSERTAR REGRISTROS EN UNA BASE DE DATOS
+    */
+
+    //INICIO RETORNAR VALORES DE UNA BASE DE DATOS
+        //return DB::table('users')->get();
+    //FIN RETORNO DE VALORES DE UNA BASE DE DATOS
+
+    //--------------------------------------FIN PRIMERA CLASE-----------------------------------------------
+
+    //-------------------------------------INICIO 2DA CLASE--------------------------------------------------
+    //CREAR Y OBTENER REGRISTROS
+
+
+
+    //-------------------------------------FIN 2DA CLASE--------------------------------------------------
+    
 
 });
